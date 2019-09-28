efeb67432a04bc683d86aad705c0d3e303827918
Anders Lindgren <anders.lindgren@payex.com>
2019-09-28 20:59:53 +0200

    Implement Exchange and Queue

--- testtool/ExchangeCommand.cs
+++ testtool/ExchangeCommand.cs
@@ -1,11 +1,27 @@
-﻿namespace RabbitREPL
+﻿using RabbitMQ.Client;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabbitREPL
 {
     internal class ExchangeCommand : ICommand
     {
         public string Description =>
             "bind exhanges and queues";
         public string DetailedDescription =>
-            @"";
+            @"Declare an Exchange
+
+Syntax:
+  exchange <exchangename> <exchangetype> <durable> <autoDelete>
+
+  where 
+    exchangename - is the name of the new exchange
+    exhangetype  - is one of direct, topic, fanout and headers
+    durable      - true if the exchange should be durable
+    autoDelete   - true if the exchange should automatically delete messages
+            
+    ";
+    
         private string[] Args { get; set; }
         private Context Context { get; set; }
 
@@ -17,7 +33,15 @@
 
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            string exhange = Args[0];
+            string type = Args[1];
+            bool durable = bool.Parse(Args[2]);
+            bool autoDelete = bool.Parse(Args[3]);
+            Context.Channel.ExchangeDeclare(
+                exchange: exhange, 
+                type: type, 
+                durable: durable, 
+                autoDelete: autoDelete);
         }
     }
 }
--- testtool/QueueCommand.cs
+++ testtool/QueueCommand.cs
@@ -3,9 +3,12 @@
     internal class QueueCommand : ICommand
     {
         public string Description =>
-            "bind exhanges and queues";
+            "Declare a Queue";
         public string DetailedDescription =>
-            @"";
+            @"Declare a Queue
+
+Syntax:
+  queue <queueName> <durable> <exclusive> <autoDelete>";
         private string[] Args { get; set; }
         private Context Context { get; set; }
 
@@ -17,7 +20,16 @@
 
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            string queueName = Args[0];
+            bool durable = bool.Parse(Args[1]);
+            bool exclusive = bool.Parse(Args[2]);
+            bool autoDelete = bool.Parse(Args[3]);
+            Context.Channel.QueueDeclare(
+                queue: queueName, 
+                durable: durable, 
+                exclusive: exclusive, 
+                autoDelete: autoDelete, 
+                arguments: null);
         }
     }
 }
